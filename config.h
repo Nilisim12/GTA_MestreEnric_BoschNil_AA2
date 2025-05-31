1d58b07ddf636a53961fc71311df510c390a2a8a
Nilisim <nil.bosch@enti.cat>
2025-05-31 23:28:14 +0200

    Peatons_agresius(enric)

---
+++ config.h
@@ -0,0 +1,24 @@
+#pragma once
+
+class Config
+{
+public:
+    int width;
+    int height;
+    int cjLife;
+    int cjAttack;
+    int sanFierroToll;
+    int lasVenturasToll;
+    int losSantosPedestrians;
+    int losSantosMaxMoney;
+    int losSantosPedestriansLife;
+    int losSantosPedestriansAttack;
+    int sanFierroPedestrians;
+    int sanFierroMaxMoney;
+    int sanFierroPedestriansLife;
+    int sanFierroPedestriansAttack;
+    int lasVenturasPedestrians;
+    int lasVenturasMaxMoney;
+    int lasVenturasPedestriansLife;
+    int lasVenturasPedestriansAttack;
+};
